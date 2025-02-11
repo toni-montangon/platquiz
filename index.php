c41ff340b924268bb79a5f1afa111ea1abd10949
toni-montangon <toni.montangon@laplateforme.io>
2025-02-11 10:22:19 +0100

    Page d'accueil + css

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PlatQuiz</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+<header>
+        <nav>
+            <div class="logo">
+                <img src="https://media.discordapp.net/attachments/1326120582391009332/1338513885941993492/Nomad.png?ex=67ab5bb2&is=67aa0a32&hm=d8f77555750424b8b35b48f2f8a1192c306d75ea226a32d6b0defd5f132ea241&=&format=webp&quality=lossless&width=625&height=625" alt="Logo quiz">
+            </div>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Catalogue</a></li>
+            </ul>
+        </nav>
+    </header>
+
+<section class="bloc1">
+    <h1>Jouez a nos Quiz !</h1>
+    <div class="blocquiz1">
+    <div class="image">
+      <img src="https://s.francaisfacile.rfi.fr/media/display/b5f7677a-6368-11ee-a106-005056bfb2b6/w:1280/p:1x1/Tour-Eiffel.jpg" alt="Image Paris">
+    </div>
+    <div class="text">
+      <h2>Capitale</h2>
+      <p></p>
+    </div>
+  </div>
+  
+  <div class="blocquiz2">
+    <div class="image">
+      <img src="https://media.discordapp.net/attachments/1244569758813126667/1338791378645418045/72EB591F-35BC-4DB1-974B-7EB1902530E2.jpg?ex=67ac5e22&is=67ab0ca2&hm=20ef41f83a0c63f23271532581933b8400861be6a57bf4190d028eba6337f2cb&=&format=webp&width=608&height=582" alt="Image voiture">
+    </div>
+    <div class="text">
+      <h2>Marque de voiture</h2>
+      <p></p>
+    </div>
+</section>
+
+<section class="bloc2">
+    <h1>Blindtest</h1>
+    <div class="blocquiz1">
+    <div class="image">
+      <img src="https://le-regisseur.com/wp-content/uploads/2023/06/comprendre-les-caracteristiques-dun-micro-scaled.jpg" alt="Image Paris">
+    </div>
+    <div class="text">
+      <h2>Musique Française</h2>
+      <p></p>
+    </div>
+  </div>
+  
+  <div class="blocquiz2">
+    <div class="image">
+      <img src="https://media.discordapp.net/attachments/1244569758813126667/1338791378645418045/72EB591F-35BC-4DB1-974B-7EB1902530E2.jpg?ex=67ac5e22&is=67ab0ca2&hm=20ef41f83a0c63f23271532581933b8400861be6a57bf4190d028eba6337f2cb&=&format=webp&width=608&height=582" alt="Image voiture">
+    </div>
+    <div class="text">
+      <h2>Marque de voiture</h2>
+      <p></p>
+    </div>
+</section>
+
+<footer>
+        <nav>
+            <ul>
+                <li><a href="C:\Users\gamin\Documents\fansite\index\index.html">Accueil</a></li>
+                <li><a href="C:\Users\gamin\Documents\fansite\index\contact\contact.html">Catalogue</a></li>
+                <li><a href="C:\Users\gamin\Documents\fansite\index\index.html">Se connecter</a></li>
+                <li><a href="C:\Users\gamin\Documents\fansite\index\contact\contact.html">S'inscrire</a></li>
+            </ul>
+        </nav>
+        <div class="socials">
+            <a href="https://www.facebook.com/yeatmusic"><img src="https://bitperfect.at/assets/blog-images/Headerbild-Was-ist-GitHub-v2.png" alt="Github"></a>
+        </div>
+        <p>&copy; 2024 Fansite Deftones. Tous droits réservés.</p>
+    </footer>
+    
+</body>
+</html>
